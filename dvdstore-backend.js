88fa364d233c5ecb8b093d3bf77e067696735e03
Julian-Hame <jhamehs@gmail.com>
2023-09-14 07:21:00 -0400

    Health check

--- dvdstore-backend.js
+++ dvdstore-backend.js
@@ -1,12 +1,11 @@
-javascript
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-const port = 3000;
 app.listen(port, () => {
-    console.log("Server listening on port ${port}");
+    console.log("Backend listening on port ${port}");
 });
